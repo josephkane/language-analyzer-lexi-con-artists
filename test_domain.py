f93d26fcd5c4203a682e6f9ff5aec598ec216217
Alan Eatherly <eatherlyae@gmail.com>
2016-07-18 15:35:47 -0500

    domain tests

--- test_domain.py
+++ test_domain.py
@@ -1,20 +1,19 @@
 import unittest
-from sentiment import *
+from behavior import *
 from tokenizer import *
 
-class test_sentiment(unittest.TestCase):
-    '''docstring for sentiment'''
-    def test_sentiment_returns_correct_values(self):
+class TestBehavior(unittest.TestCase):
+    '''docstring for behavior'''
+    def test_behavior_returns_correct_values(self):
         token_data = TokenData()
         token_data.message_dict = {'p':2, 'r':2, 's':3, 't':2, 'y':1, 'a':3, 'd':2, 'e':7, 'f':1, 'h':2, 'i':5, 'k':2, 'l':1, 'm':2, 'n':3, 'o':1}
         token_data.word_list = ['Friendship', 'is', 'like', 'money', 'easier', 'made', 'than', 'kept']
         token_data.word_list_lower = ['friendship', 'is', 'like', 'money', 'easier', 'made', 'than', 'kept']
         token_data.punctuation = {',':1, '.':1}
         classified_data = ClassifiedData()
-        classified_data.count = 4
-        classified_data.subCount = {'positive':3, 'neutral':1, 'negative':0}
-        sentiment = Sentiment()
-        classified_message = sentiment.categorize_message(token_data)
+        classified_data.count = 2
+        classified_data.subCount = {'financial':1, 'behavioral':0, 'scientific':0, 'educational':0, 'politics':0, 'relationships':1}
+        behavior = Behavior()
+        classified_message = behavior.categorize_message(token_data)
         self.assertEqual(classified_message.count, classified_data.count)
         self.assertEqual(classified_message.subCount, classified_data.subCount)
-
