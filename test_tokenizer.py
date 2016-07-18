b5521dff9903e859984c41443fd8537ffa1afbd0
Michael Mead <michaelmead007@icloud.com>
2016-07-18 14:31:20 -0500

    add behavior test

--- test_tokenizer.py
+++ test_tokenizer.py
@@ -8,12 +8,14 @@ class test_tokenizer(unittest.TestCase):
         token_data = tokenize(message)
         message_dict = {'p':2, 'r':2, 's':3, 't':2, 'y':1, 'a':3, 'd':2, 'e':7, 'f':1, 'h':2, 'i':5, 'k':2, 'l':1, 'm':2, 'n':3, 'o':1}
         word_list = ['Friendship', 'is', 'like', 'money', 'easier', 'made', 'than', 'kept']
+        word_list_lower = ['friendship', 'is', 'like', 'money', 'easier', 'made', 'than', 'kept']
         punctuation = {',':1, '.':1}
         self.assertEqual(token_data.alphanum_char, message_dict)
         self.assertEqual(token_data.word_count, 8)
         self.assertEqual(token_data.word_positions, word_list)
         self.assertEqual(token_data.sentence_count, 1)
         self.assertEqual(token_data.punctuation, punctuation)
+        self.assertEqual(token_data.word_list_lower_case, word_list_lower)
 
     def test_alphanum_char_returns_number_of_chars(self):
         '''docstring for test_alpha_num_chars'''
