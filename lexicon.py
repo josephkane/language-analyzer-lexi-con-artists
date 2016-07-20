418b43422dc6c86798c8b2c714801a81bdc994fa
Joseph Kane <josephckane@gmail.com>
2016-07-20 09:50:13 -0500

    refactor categorize method to account for word in multiple subcategories

--- lexicon.py
+++ lexicon.py
@@ -5,13 +5,15 @@ class Lexicon:
 
     def categorize_message(self, msg):
         data  = ClassifiedData()
-        for key, value in self.lexicon.items():
-         data.subcount[key] = 0
-         for k, v in value.items():
-             for word in msg.word_list:
-                 if k == word:
-                    data.count += v
-                    data.subcount[key] += v
+        data.subcount = dict.fromkeys(self.lexicon.keys(), 0)
+        for word in msg.word_list:
+            word_added = False
+            for subcategory, entries in self.lexicon.items():
+                if word in entries.keys():
+                    if not word_added:
+                        data.count += 1
+                        word_added = True
+                    data.subcount[subcategory] += 1
 
         return data
 
