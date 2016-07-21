1c81e56b4819cf9d786ca73ab522d26f1e3a6d17
Michael Mead <michaelmead007@icloud.com>
2016-07-21 15:56:13 -0500

    tests for project X passing

--- test_analyzer.py
+++ test_analyzer.py
@@ -38,24 +38,24 @@ class TestAnalyzer(unittest.TestCase):
 Message:  Class is an aura of confidence that is being sure without being cocky. Class has nothing to do with money. Class never runs scared. It is self-discipline and self-knowledge. It's the sure-footedness that comes with having proved you can meet life.
 
 # behavior #
-- aggressive     [|---------]  0.1
-- inquisitive    [||||||||--]  0.8
-- mentoring      [||||||----]  0.6
-- passive        [||||------]  0.4
-- transaction    [|||-------]  0.3
+- aggressive     [\U0001F621 - - - - - - - - - ]  0.1
+- inquisitive    [\U0001F914 \U0001F914 \U0001F914 \U0001F914 \U0001F914 \U0001F914 \U0001F914 \U0001F914 - - ]  0.8
+- mentoring      [\U0001F913 \U0001F913 \U0001F913 \U0001F913 \U0001F913 \U0001F913 - - - - ]  0.6
+- passive        [\U0001F910 \U0001F910 \U0001F910 \U0001F910 - - - - - - ]  0.4
+- transaction    [\U0001F4B8 \U0001F4B8 \U0001F4B8 - - - - - - - ]  0.3
 
 # domain #
-- behavioral     [||--------]  0.2
-- educational    [||||------]  0.4
-- financial      [|---------]  0.1
-- politics       [|---------]  0.1
-- relationships  [|---------]  0.1
-- scientific     [|||||||---]  0.7
+- behavioral     [\U0001F607 \U0001F607 - - - - - - - - ]  0.2
+- educational    [\U0001f4DA \U0001f4DA \U0001f4DA \U0001f4DA - - - - - - ]  0.4
+- financial      [\U0001F4B0 - - - - - - - - - ]  0.1
+- politics       [\U0001F5FD - - - - - - - - - ]  0.1
+- relationships  [\U0001F48F - - - - - - - - - ]  0.1
+- scientific     [\U0001F52C \U0001F52C \U0001F52C \U0001F52C \U0001F52C \U0001F52C \U0001F52C - - - ]  0.7
 
 # sentiment #
-- negative       [||--------]  0.2
-- neutral        [|||-------]  0.3
-- positive       [|||||-----]  0.5
+- negative       [\U0001F612 \U0001F612 - - - - - - - - ]  0.2
+- neutral        [\U0001F636 \U0001F636 \U0001F636 - - - - - - - ]  0.3
+- positive       [\U0001F600 \U0001F600 \U0001F600 \U0001F600 \U0001F600 - - - - - ]  0.5
 
 '''
 
